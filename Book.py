2bda53e466b98c1d7b6f36f0d48f761b7e53bc0c
Daniyal Asif <daniyalasif@Daniyals-MacBook-Air.local>
2025-07-22 16:25:20 +0500

    made isbn, title private and getters and setters for isbn

--- Book.py
+++ Book.py
@@ -1,9 +1,9 @@
 class BOOK():
     def __init__(self, title, author, publication_year,isbn,availability_status):
-        self.title = title
+        self.__title = title
         self.author = author
         self.publication_year = publication_year
-        self.isbn = isbn
+        self.__isbn = isbn
         self.availability_status = availability_status
     
     def get_book_info(self):
@@ -16,9 +16,25 @@ class BOOK():
     
     def update_availability(self, status):
         self.availability_status = status
+    
+    def getISBN(self):
+        return self.__isbn
+    
+    def setISBN(self,isbn):
+        #isbn ="388439"
+        if (isbn.__len__ != 13):
+            print("ISBN should be 13 digits long.")
+        else:
+            self.__isbn = isbn
+
+
+
+
+
 
     def __str__(self):
         return self.title
+    
 
 
 if __name__ == "__main__":
