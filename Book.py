908ff4f498f584d945d1ecb5a092f623a1a5a016
Daniyal Asif <daniyalasif@Daniyals-MacBook-Air.local>
2025-07-22 15:00:01 +0500

    Mde Constructor for Book class

---
+++ Book.py
@@ -0,0 +1,8 @@
+class Book():
+    def __init__(self, title, author, publication_year,isbn,availability_status):
+        self.title = title
+        self.author = author
+        self.publication_year = publication_year
+        self.isbn = isbn
+        self.availability_status = availability_status
+    
